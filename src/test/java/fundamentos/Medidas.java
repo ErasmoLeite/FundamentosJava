10d3a64525a35a819f65eef5fca86d093a609234
Erasmo Leite <erasmo.ldasilva@hotmail.com>
2021-09-18 22:44:42 -0300

    Commit Inicial - Criamos o método main e a função para calcular a área do quadrado.

---
+++ src/test/java/fundamentos/Medidas.java
@@ -0,0 +1,46 @@
+// 1- Pacote
+package fundamentos;
+
+// 2 - Bibliotecas
+import java.util.Scanner;
+
+// 3- Classes
+public class Medidas {
+    // 3.1 - Atributos
+    static Scanner entrada = new Scanner(System.in);;
+
+    // 3.2 - Métodos e Funções
+    public static void main(String[] args) throws IllegalAccessException {
+        String opcao;
+        int area = 0; // receber o resultado dos cálculos de áreas
+
+        System.out.println("Escolha o Calculo Desejado");
+        System.out.println("(1) - Area do Quadrado");
+        System.out.println("(2) - Area do Retangulo");
+        System.out.println("(3) - Area do Triangulo");
+        System.out.println("(4) - Area do Circulo");
+
+        opcao = entrada.nextLine(); // leitura da opção
+        switch (opcao) {
+            case "1":
+                area = calcularAreaDoquadrado();
+            case "2":
+                //Todo calcular área do retangulo
+                break;
+            default:
+                System.out.println("Opcao Invalida: " + opcao);
+
+        }
+        if (area > 0) {
+            System.out.println("A Area e de " + area + "m²");
+        }
+    }
+
+    public static int calcularAreaDoquadrado(){
+        int lado; // tamanho do lado do quadrado
+
+        System.out.println("Digite o tamanho do lado: ");
+        lado = entrada.nextInt();  // leitura do tamanho do lado
+        return lado = lado * lado;   // retorna a área do quadrado
+    }
+}
